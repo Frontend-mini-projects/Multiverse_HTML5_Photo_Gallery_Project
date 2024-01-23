21086f5afa630acc1d6e80edbb1ca3b2f1e6b7a1
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2024-01-24 00:53:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert('working')
+document.getElementById('menu').addEventListener('click',()=>{
+    document.getElementById('mobile-menu').classList.toggle('d-none');
+    
+});
+document.getElementById('close').addEventListener('click',()=>{
+    document.getElementById('mobile-menu').classList.toggle('d-none');
+    
+})
